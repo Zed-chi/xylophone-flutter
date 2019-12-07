cb0fcc2f094998d01309f7192ffab901353a1d54
zed <zed@mail.ru>
2019-12-07 22:18:12 +0300

    added first button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -7,8 +9,17 @@ class XylophoneApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(),
         body: SafeArea(
-          child: Container(),
+          child: Container(
+            child: FlatButton(
+              onPressed: (){
+                final player = AudioCache();
+                player.play("note1.wav");
+              },
+              child: Text("data"),
+            ),
+          ),
         ),
       ),
     );
