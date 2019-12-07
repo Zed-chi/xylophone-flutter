996755fc7cac51ca7cb11178842e7eab185670ab
zed <zed@mail.ru>
2019-12-07 23:02:49 +0300

    fix buttons

--- lib/main.dart
+++ lib/main.dart
@@ -5,20 +5,39 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int num){
+    final player = AudioCache();
+    player.play("note$num.wav");
+  }
+
+  Expanded BuildKey(int num){
+    return Expanded(
+        child: FlatButton(
+        color: Colors.red,
+        onPressed: (){playSound(num);},                  
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(),
+        backgroundColor: Colors.black,
+        
         body: SafeArea(
-          child: Container(
-            child: FlatButton(
-              onPressed: (){
-                final player = AudioCache();
-                player.play("note1.wav");
-              },
-              child: Text("data"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              
+              BuildKey(1),
+              BuildKey(2),
+              BuildKey(3),
+              BuildKey(4),
+              BuildKey(5),
+              BuildKey(6),
+              BuildKey(7),              
+
+            ],
           ),
         ),
       ),
